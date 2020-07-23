ddacedf3875563adcdbae7b61f4c2ca2bb6c4cf0
iam-ghost <64468136+iam-ghost@users.noreply.github.com>
2020-07-23 13:38:30 +0530

    success

---
+++ config.sh
@@ -0,0 +1,11 @@
+docker start kubectl_portal
+if docker exec kubectl_portal kubectl get pods | grep server
+	then
+	docker exec kubectl_portal kubectl replace -f server.yaml
+	else
+	docker exec kubectl_portal kubectl create -f server.yaml
+  sleep 50
+	fi
+	POD=$(kubectl get pod -l app=httpd -o jsonpath="{.items[0].metadata.name}")
+	
+    kubectl cp *.html $POD:/var/www/html/
